292a2a342d1c1302a7a3dede47e3807da6dff628
Prince Kumar <princepk7878kumar@gmail.com>
2023-09-24 18:15:31 +0530

    javascript

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+    3);  // code readability should be high
+
+console.log("Knight");
+
+let name="Knight"
+let age=18
+let isLoggedIn=false
+let state;
+
+//Datatypes
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//symbol => unqiue
+
+//object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);// object
+
---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,72 @@
+let score = "kngiht"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" =>33
+//"33abc" => Nan
+//true => 1; false =>0
+
+
+let isLoggedIn ="hitesh"
+
+let booleanIsloggedIn=Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+//1 =>true; 0 => false
+// "" => false
+//"hitesh" => true
+
+let someNumber =33
+
+let stringNumber =String(someNumber)
+console.log(typeof stringNumber);
+
+
+//********************************Operation*********************** */
+
+let value =3
+let negVaue = -value
+console.log(negVaue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1="hello"
+let str2="knght"
+
+let str3=str1 + str2
+console.log(str3);
+
+console.log("1" +2);
+console.log(1+"2");
+console.log("1"+2+"2");
+console.log(1+2+"2");
+
+console.log((3+4)*5 %3);
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter =100
+
+++gameCounter
+console.log(gameCounter);
+
+
+
+
